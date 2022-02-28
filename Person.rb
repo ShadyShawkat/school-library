6ac75e6c304d26281365abc9f2af09c30c9c68f2
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-28 19:54:30 +0200

    Optimize Person class code

--- Person.rb
+++ Person.rb
@@ -1,7 +1,6 @@
 class Person
   attr_reader :id
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name,  :age
   def initialize(age, name: "Unknown", parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
