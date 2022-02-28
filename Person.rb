7be028bd69f42ba405622477bd0b8052370fc542
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-28 20:05:18 +0200

    Fix linters

--- Person.rb
+++ Person.rb
@@ -1,14 +1,17 @@
 class Person
   attr_reader :id
-  attr_accessor :name,  :age
-  def initialize(age, name: "Unknown", parent_permission: true)
+  attr_accessor :name, :age
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  private def is_of_age?
+  private
+
+  def of_age?
     @age >= 18
   end
 
--- Student.rb
+++ Student.rb
@@ -1,7 +1,7 @@
-require_relative 'Person'
+require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name: "Unknown", parent_permission: true)
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
