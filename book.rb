3b11b8c5161240f3adae6435465a613914e35d97
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-02 20:16:40 +0200

    Add a 'book has many rentals' relationship

--- book.rb
+++ book.rb
@@ -1,7 +1,15 @@
+require_relative 'rental'
+
 class Book
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title, @author = title, author
+    @rentals = []
+  end
+
+  def rent(date)
+    @rentals << Rental.new(date)
   end
 end
