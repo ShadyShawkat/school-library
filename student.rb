2a3b1cb85d1532d50518b51bbdd26923e878926e
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-02 19:52:09 +0200

    Add a 'student belongs to a classroom' relationship

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def join_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
