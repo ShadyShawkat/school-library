74cbf1ae5fe32c631e5726d457046ef7ea13c45f
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-03 18:09:10 +0200

    Create display_options method

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 class App
+  def display_options
+    puts "1 - List all books"
+    puts "2 - List all people"
+    puts "3 - Create a person"
+    puts "4 - Create a book"
+    puts "5 - Create a rental"
+    puts "6 - List all rentals for a given person id"
+    puts "7 - Exit"
+  end
+
   def run
-    puts('asd')
+    display_options()
   end
 end
 
